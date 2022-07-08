9557088dbdfb8c97ff53f78b63d758c723665f9f
kjw <kjw@kjwui-MacBook-Pro.local>
2022-07-08 14:00:57 +0900

    feat: custom hook test

--- src/components/CustomElement.js
+++ src/components/CustomElement.js
@@ -3,11 +3,12 @@ import useData from '@/hook/useData';
 import Custom2 from './Custom2';
 
 const useJw = () => {
-  const data = useData({ k: 7 });
+  const data = useData({ k: 7, j: 1 });
   const data2 = useData({ k: 1 });
 
   const handle = () => {
     data.k += 1;
+    data.j += 1;
   };
   const handle2 = () => {
     data2.k += 1;
@@ -18,18 +19,26 @@ const useJw = () => {
 
 export default function CustomElement({ props, children }) {
   const { data, data2, handle, handle2 } = useJw();
+  const data3 = useData({ k: 1 });
+
+  const handle3 = () => {
+    data3.k += 1;
+  };
 
   const componentMaker = () => {
     return (
-      <div class={`aa`}>
-        <span onClick={handle}>vava{data.k}aa</span>
-        <span onClick={handle2}>{data2.k}-vava</span>
+      <div class={`aa${data.k}`}>
+        <button onClick={handle}>vava{data.k}aa</button>
+        <button onClick={handle2}>{data2.k}-vava</button>
+        <button onClick={handle3}>{data3.k}-vava</button>
         {data.k % 2 === 0 ? <span>j</span> : null}
         <Custom2 k={data.k} />
         <Custom2 k={data.k + 1} />
         <div>
-          <div>{data.k}</div>
-          <div>{data2.k}</div>
+          <div>data.k: {data.k}</div>
+          <div>data.j: {data.j}</div>
+          <div>data2.k: {data2.k}</div>
+          <div>data3.k: {data3.k}</div>
         </div>
         <br />
         <br />
