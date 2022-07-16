01791af038ed88ba6c104fd4e5a15a87068b9770
kjw <kjw@kjwui-MacBook-Pro.local>
2022-07-17 01:57:28 +0900

    feat: add example page

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,12 +1,14 @@
 import { h, Fragment } from '@/wDom';
-import { makeRef, updated, makeData } from '@/hook';
+import { makeRef, updated, makeData, useDataStore } from '@/hook';
 import CustomElement from '@/components/CustomElement';
 
 export default function NestedFragment({ props, children }) {
+  const exampleData = useDataStore('example');
+
   const componentMaker = () => {
     return (
       <section>
-        <h1>나만의 커스텀 프레임웍 제작기</h1>
+        <h1>{exampleData.title}</h1>
         <ol>
           <li>ss</li>
           <li>ss</li>
