4a3499c1d5d14851f9a5a486aa306b874a06410d
superlucky84 <superlucky84@gmail.com>
2022-07-21 17:25:12 +0900

    feat: vite를 위한 eslint플러그인 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 export default defineConfig({
+  plugins: [eslintPlugin({ eslintOptions: { cache: false } })],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
