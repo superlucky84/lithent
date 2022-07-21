2ea1c043ae628ab51e8a97946827ee87bc3e41d6
superlucky84 <superlucky84@gmail.com>
2022-07-21 18:38:10 +0900

    feat: 빌드툴 변경작업 webpack -> vite (#12)
    * feat: 빌드툴 변경작업 webpack -> vite
    
    * feat: vite를 위한 eslint플러그인 추가
    
    * feat: vite multi build

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
+
+export default ({ path, name, fileName }) => {
+  return defineConfig({
+    plugins: [eslintPlugin({ eslintOptions: { cache: false } })],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      emptyOutDir: false,
+      lib: {
+        entry: resolve(__dirname, path),
+        name,
+        fileName,
+      },
+    },
+    esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment',
+    },
+  });
+};
