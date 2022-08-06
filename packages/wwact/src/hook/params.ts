b989d2af1af48ab167e3129f8dd81c3da4e51339
superlucky84 <superlucky84@gmail.com>
2022-08-06 23:15:01 +0900

    fix: removed unnessary file

--- packages/wwact/src/hook/params.ts
+++
@@ -1,9 +0,0 @@
-import { routerParams } from '@/helper/universalRef';
-
-export default function useParams() {
-  return routerParams.value;
-}
-
-export function addParams(key: string, value: string) {
-  routerParams.value[key] = value;
-}
