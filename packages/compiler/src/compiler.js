09e98361072f94451386b9dfe25c012c2b29bb0c
superlucky84 <superlucky84@gmail.com>
2022-08-10 00:57:44 +0900

    템플릿 파싱 - 1차 태그 트리 만들기 프로토타이핑

--- packages/compiler/src/compiler.js
+++ packages/compiler/src/compiler.js
@@ -1,7 +1,7 @@
 const j = `
 <div class={\`aaaaaaaaa$\{data.k\}\`} jj = "2" />
 <div class={\`aaaaaaaaa$\{data.k\}\`} jj = "2">
-  <button onClick={handle}> !vava{data.k}aa</button>
+  <button w-if={true} onClick={handle}> !vava{data.k}aa</button>
   <button onClick={handle2}>{data2.k}-vava</button>
   <button onClick={handle3}>{data3.k}-vava</button>
   <div ref={domRef}>
@@ -13,23 +13,91 @@ const j = `
   <br />
   <br />
 </div>
-`;
+`.trim();
+
+console.log(j);
+
 const jLength = j.length;
+
+const result = {
+  children: [],
+};
+const sa = {
+  start: 0,
+  end: 0,
+  code: j,
+};
 
 function isExisty(value) {
   return value !== undefined && value !== null;
 }
 
-function getString(s, start, end) {
+function search(targetString) {
   let acc = '';
-  if (isExisty(start) && isExisty(end)) {
-    for (let i = start; i < end; i += 1) {
-      acc += s[i];
+  let finded = false;
+
+  for (sa.end = sa.start; sa.end < sa.code.length; sa.end += 1) {
+    if (sa.code[sa.end] === targetString) {
+      finded = true;
+      sa.start = sa.end;
+      break;
     }
+
+    acc += sa.code[sa.end];
   }
 
-  return acc;
+  return [finded, acc.trim()];
 }
 
-const jj = getString(j.trim(), 0, 13);
-console.log(jj);
+const tagStack = [];
+
+function find(parent) {
+  const [finded, acc1] = search('<');
+  if (finded) {
+    const innerValue = acc1.replace(/>/, '');
+    if (innerValue) {
+      const item = {
+        text: innerValue,
+        parent,
+      };
+      parent.children.push(item);
+    }
+    sa.start += 1;
+
+    const [finded, acc] = search('>');
+    if (finded) {
+      const children = [];
+      const [tagName] = acc.split(' ');
+
+      if (!/^\//.test(tagName)) {
+        const hasChildren = !/\/$/.test(acc);
+        const item = {
+          tagName,
+          s: acc,
+          hasChildren,
+          children,
+          parent,
+        };
+        tagStack.push(tagName);
+
+        parent.children.push(item);
+
+        return hasChildren ? item : parent;
+      } else {
+        return parent.parent;
+      }
+    }
+  }
+}
+
+function makeTree(parentArr = { children: [] }) {
+  const returnArr = find(parentArr);
+  if (returnArr) {
+    return makeTree(returnArr);
+  }
+
+  return parentArr;
+}
+const result2 = makeTree();
+
+console.log(result2);
