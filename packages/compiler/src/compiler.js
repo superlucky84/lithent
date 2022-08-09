5c9462975b36010da40affc35c2d54394fbfe24a
superlucky84 <superlucky84@gmail.com>
2022-08-09 18:18:27 +0900

    compiler 파서 구현방식 변경

--- packages/compiler/src/compiler.js
+++ packages/compiler/src/compiler.js
@@ -1,7 +1,7 @@
 const j = `
 <div class={\`aaaaaaaaa$\{data.k\}\`} jj = "2" />
 <div class={\`aaaaaaaaa$\{data.k\}\`} jj = "2">
-  <button onClick={handle}>!vava{data.k}aa</button>
+  <button onClick={handle}> !vava{data.k}aa</button>
   <button onClick={handle2}>{data2.k}-vava</button>
   <button onClick={handle3}>{data3.k}-vava</button>
   <div ref={domRef}>
@@ -14,75 +14,22 @@ const j = `
   <br />
 </div>
 `;
+const jLength = j.length;
 
-const templateArr = j.split('');
-const va = makeFinder(templateArr);
-
-console.log(templateArr);
-
-function* makeFinder(templateArr) {
-  const target = [...templateArr];
-  while (target.length > 0) {
-    const item = target.shift();
-
-    yield item;
-  }
+function isExisty(value) {
+  return value !== undefined && value !== null;
 }
 
-function search(regex) {
-  let i;
-  let find = false;
+function getString(s, start, end) {
   let acc = '';
-  do {
-    i = va.next();
-    acc = acc + i.value;
-    if (regex.test(i.value)) {
-      find = true;
+  if (isExisty(start) && isExisty(end)) {
+    for (let i = start; i < end; i += 1) {
+      acc += s[i];
     }
-  } while (!i.done && !find);
+  }
 
   return acc;
 }
 
-function findTag(parentArr) {
-  const s = search(/[^\n\r]/).trim();
-  if (s === '<') {
-    const tagName = search(/ /).trim();
-    const etc = search(/\/?>/).replace(/>$/, '').trim();
-    const hasChildren = !/\/$/.test(etc);
-    const children = [];
-    console.log('ISTAG', tagName, etc, hasChildren);
-
-    const item = {
-      tagName,
-      hasChildren,
-      children,
-      etc,
-    };
-
-    console.log(parentArr);
-    parentArr.push(item);
-
-    const newParentArr = hasChildren ? children : parentArr;
-
-    return newParentArr;
-  } else {
-    const text = search(/[ |{]/).trim();
-
-    console.log('ISNOTTAG', s + text);
-
-    return parentArr;
-  }
-}
-
-const root = [];
-let tempParentArr = [];
-
-tempParentArr = findTag(root);
-tempParentArr = findTag(tempParentArr);
-tempParentArr = findTag(tempParentArr);
-tempParentArr = findTag(tempParentArr);
-tempParentArr = findTag(tempParentArr);
-tempParentArr = findTag(tempParentArr);
-
-console.log('RESULT = ', root);
+const jj = getString(j.trim(), 0, 13);
+console.log(jj);
