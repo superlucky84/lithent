0daa68d40e8b80245d9f5a8746c631ff5f393b36
superlucky84 <superlucky84@gmail.com>
2023-06-07 01:22:40 +0900

    feat: make function interface change

--- packages/wwact/src/Wwact.ts
+++ packages/wwact/src/Wwact.ts
@@ -1,18 +1,30 @@
 import { WDom } from '@/index';
 import { Param } from '@/types';
 
-export default function make<Props, Member extends {}>(
-  mounter: (info: Omit<Param<Props, Member>, 'children'>) => Member,
-  template: (info: Param<Props, Member>) => WDom
+type Option<Props, Member> = (info: Param<Props, Member>) => Member;
+
+export default function make<Props, Member extends {} | null>(
+  first: Option<Props, Member>,
+  second?: Option<Props, Member>
 ) {
+  let mounter: null | Option<Props, Member>;
+  let template: Option<Props, Member>;
+
+  if (second) {
+    mounter = first as Option<Props, Member>;
+    template = second as Option<Props, Member>;
+  } else {
+    mounter = null;
+    template = first as Option<Props, Member>;
+  }
+
   return function (props: Props, children: WDom[]) {
     const member = {} as Member;
-
-    if (mounter) {
-      Object.assign(member, mounter({ props, member }));
-    }
-
     const info = { props, member, children };
+
+    if (mounter && member) {
+      Object.assign(member, mounter({ props, member, children }));
+    }
 
     return () => template(info);
   };
