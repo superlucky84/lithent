c2311ba748279e85f6e6dbbacdba745fda83d2c6
superlucky84 <superlucky84@gmail.com>
2023-06-09 11:28:21 +0900

    chore: version up 0.20.0

--- packages/wwact/types/hook/globalStore.d.ts
+++ packages/wwact/types/hook/globalStore.d.ts
@@ -1,3 +1,3 @@
 import { UseDataStoreValue } from '../types';
 export declare const globalStore: <T extends {}>(storeKey: string) => T;
-export declare const makeSharedUpdater: <T extends {}>(storeKey: string, initValue: T) => UseDataStoreValue;
+export declare const makeGlobalStore: <T extends {}>(storeKey: string, initValue: T) => UseDataStoreValue;
