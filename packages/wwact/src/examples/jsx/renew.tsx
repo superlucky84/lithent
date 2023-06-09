442bd5d9f71ec898f790e52f5e656fa73b46758e
superlucky84 <superlucky84@gmail.com>
2023-06-09 17:23:34 +0900

    feat: new type state mutation

--- packages/wwact/src/examples/jsx/renew.tsx
+++ packages/wwact/src/examples/jsx/renew.tsx
@@ -1,18 +1,21 @@
 // example.jsx
-import { h, Fragment, render, Props, WDom, Renew } from '@/index';
+import { h, Fragment, render, Props, WDom, Renew } from 'wwact';
+import { state2 } from '@/wwactStore';
 
-function Renew(_props: Props, _children: WDom[], renew: Renew) {
+function Renew(_props: Props, _children: WDom[], _renew: Renew) {
   let count1 = 0;
   let count2 = 0;
   let count3 = 0;
   let count4 = 0;
+  const count5 = state2<number>(7);
 
   const change = () => {
     count1 += 1;
     count2 += 2;
     count3 += 3;
     count4 -= 1;
-    renew();
+    count5.set(count5.val * 2);
+    // renew();
   };
 
   return () => (
@@ -21,6 +24,7 @@ function Renew(_props: Props, _children: WDom[], renew: Renew) {
       <li>count2: {count2}</li>
       <li>count3: {count3}</li>
       <li>count4: {count4}</li>
+      <li>number: {count5.v}</li>
       <button onClick={change}>change</button>
     </Fragment>
   );
--- packages/wwact/src/wwactStore.ts
+++ packages/wwact/src/wwactStore.ts
@@ -1,2 +1,3 @@
 export { globalStore, makeGlobalStore } from '@/hook/globalStore';
 export { localStore } from '@/hook/localStore';
+export { state2 } from '@/hook/state2';
