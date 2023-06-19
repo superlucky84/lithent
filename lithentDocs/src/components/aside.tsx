7deef01aa2d728907d3a5393249afa795c095eba
superlucky84 <superlucky84@gmail.com>
2023-06-20 07:30:11 +0900

    docs link change

--- lithentDocs/src/components/aside.tsx
+++ lithentDocs/src/components/aside.tsx
@@ -23,7 +23,7 @@ export const Aside = mount<{ isHidden: boolean }>(() => {
               </li>
               <li>
                 <a
-                  href="/#"
+                  href="#main"
                   target="_self"
                   class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
                   innerHTML={`<svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"> <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path> <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" ></path> </svg> <span class="ml-3" sidebar-toggle-item=""> Basic Guide </span>`}
--- lithentDocs/src/components/headernav.tsx
+++ lithentDocs/src/components/headernav.tsx
@@ -24,10 +24,7 @@ export const HeaderNav = mount(r => {
                 shardStore.showHiddenMenu ? '' : 'hidden'
               }" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" ></path> </svg>`}
             ></button>
-            <a
-              href="https://github.com/superlucky84/lithent"
-              class="flex ml-2 md:mr-24"
-            >
+            <a href="#main" class="flex ml-2 md:mr-24">
               <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
                 Lithent
               </span>
