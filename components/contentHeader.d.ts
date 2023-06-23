242280a4e7fafe05bd7a37e64bfcfa048979e6af
superlucky84 <superlucky84@gmail.com>
2023-06-24 00:34:59 +0900

    chore: document update for install page

--- components/contentHeader.d.ts
+++ components/contentHeader.d.ts
@@ -1,5 +1,11 @@
 export declare const ContentHeader: (_props: {
+    taregetRef?: {
+        value: HTMLElement | null;
+    } | undefined;
     title: string;
 }, _children: import("lithent").WDom[]) => import("../../../dist/types").Component<{
+    taregetRef?: {
+        value: HTMLElement | null;
+    } | undefined;
     title: string;
 }>;
