82c6ead9505e2deca766a4472fe0a3888ed95f7c
superlucky84 <superlucky84@gmail.com>
2023-06-27 03:40:42 +0900

    feat: remove unnessacry file

--- lithentDocs/vite.config.js
+++ lithentDocs/vite.config.js
@@ -2,15 +2,11 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import eslintPlugin from '@nabla/vite-plugin-eslint';
-import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
     checker({ typescript: true }),
     eslintPlugin({ eslintOptions: { cache: false } }),
-    dts({
-      outputDir: ['dist'],
-    }),
   ],
   resolve: {
     alias: {
