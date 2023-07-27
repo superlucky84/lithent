cadb724c4d910969435d351e38311ffa757246f2
superlucky84 <superlucky84@gmail.com>
2023-07-27 20:41:26 +0900

    fixed: sorted update element renew bug

--- src/render.ts
+++ src/render.ts
@@ -122,14 +122,9 @@ const typeSortedReplace = (newWDom: WDom) => {
 };
 
 const typeSortedUpdate = (newWDom: WDom) => {
-  // typeDelete(newWDom);
-  // typeAdd(newWDom, newWDom.el);
   typeUpdate(newWDom);
-  console.log(newWDom);
 
-  const newElement = newWDom.el;
-  console.log('123', newElement);
-  /*
+  const newElement = getElementFromFragment(newWDom);
   const parentWDom = getParent(newWDom);
   if (parentWDom.type) {
     const parentEl = findRealParentElement(parentWDom);
@@ -141,15 +136,27 @@ const typeSortedUpdate = (newWDom: WDom) => {
 
     if (newElement && parentEl) {
       if (nextEl) {
-        console.log('a');
         parentEl.insertBefore(newElement, nextEl);
       } else {
-        console.log('b', parentEl, newElement?.children);
         parentEl.appendChild(newElement);
       }
     }
   }
-  */
+};
+
+const getElementFromFragment = (newWDom: WDom) => {
+  if (checkVirtualType(newWDom.type)) {
+    return (newWDom?.children || []).reduce((acc, item) => {
+      const element = getElementFromFragment(item);
+
+      if (element) {
+        acc.appendChild(element);
+      }
+      return acc;
+    }, DF());
+  }
+
+  return newWDom.el;
 };
 
 const typeAdd = (
@@ -163,11 +170,7 @@ const typeAdd = (
   const parentWDom = getParent(newWDom);
   if (parentWDom.type) {
     const parentEl = findRealParentElement(parentWDom);
-    const isLoop = parentWDom.type === 'loop';
-    const nextEl =
-      isLoop && parentWDom.needRerender
-        ? startFindNextBrotherElement(parentWDom, getParent(parentWDom))
-        : startFindNextBrotherElement(newWDom, parentWDom);
+    const nextEl = startFindNextBrotherElement(newWDom, parentWDom);
 
     if (newElement && parentEl) {
       if (nextEl) {
