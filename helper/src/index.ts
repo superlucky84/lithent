6f280bf69ebbdee3a3b95e356e31742da6989ceb
superlucky84 <superlucky84@gmail.com>
2023-09-04 00:47:52 +0900

    feat: addad helper cacheUpdate

--- helper/src/index.ts
+++ helper/src/index.ts
@@ -2,4 +2,5 @@ export { state } from '@/hook/state';
 export { computed } from '@/hook/computed';
 export { effect } from '@/hook/effect';
 export { store } from '@/hook/store';
+export { cacheUpdate } from '@/hook/cacheUpdate';
 export { nextTickRender } from '@/hook/nextTickRender';
