7f9a2c897bf9b0ddf2a520c2f0b03dea3a361bea
superlucky84 <superlucky84@gmail.com>
2024-05-25 01:06:31 +0900

    feat: vite version up

--- ftags/vite.config.js
+++ ftags/vite.config.js
@@ -1,13 +1,13 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
+import eslint from 'vite-plugin-eslint';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
     checker({ typescript: true }),
-    eslintPlugin({ eslintOptions: { cache: false } }),
+    eslint(),
     dts({
       outputDir: ['dist'],
     }),
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
-import eslintPlugin from '@nabla/vite-plugin-eslint';
+import eslint from 'vite-plugin-eslint';
 import dts from 'vite-plugin-dts';
 export default defineConfig({
   plugins: [
     checker({ typescript: true }),
-    eslintPlugin({ eslintOptions: { cache: false } }),
+    eslint(),
     dts({
       outputDir: ['dist'],
     }),
@@ -17,7 +17,6 @@ export default defineConfig({
     },
   },
   build: {
-    minify: 'terser',
     emptyOutDir: false,
     sourcemap: true,
     lib: {
