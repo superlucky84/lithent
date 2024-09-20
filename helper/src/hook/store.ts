b5f71a3ecc8838676a1c8f846af140bbe3648075
superlucky84 <superlucky84@gmail.com>
2024-09-21 01:51:26 +0900

    Feat/store cached (#61)
    * feat: Fixed bug by preventing adding and updating Store features.
    
    * feat: added cache option

--- helper/src/hook/store.ts
+++ helper/src/hook/store.ts
@@ -2,82 +2,97 @@
  * DataStore
  */
 type Renew<T> = (store: T) => boolean | AbortSignal | void;
+type Run = () => boolean | AbortSignal | void;
 type StoreType<V> = V extends { [key: string]: unknown } ? V : { value: V };
-type StoreValue<T> = {
-  [key: string | symbol]: Renew<T>[];
+type StoreValue = {
+  [key: string | symbol]: Set<Run>;
 };
-type StoreRenderObserveSubList<T> = StoreValue<T>;
+
+const DEFAULT_OPTION = { cache: true };
 
 export const store = <V>(initialValue: V) => {
   type T = StoreType<V>;
 
   const allowInitSetting = { value: false };
   const isObjectTypeValue =
-    typeof initialValue === 'object' && initialValue !== null;
+    !Array.isArray(initialValue) &&
+    typeof initialValue === 'object' &&
+    initialValue !== null;
   const value: T = isObjectTypeValue
     ? (initialValue as T)
     : ({ value: initialValue } as T);
 
-  let storeRenderList: Renew<T>[] = [];
-  const storeRenderObserveList: StoreRenderObserveSubList<T>[] = [];
+  const storeRenderList: Set<Run> = new Set();
+  const storeRenderObserveList: StoreValue[] = [];
+  const cacheMap = new WeakMap<Renew<T>, T>();
 
-  return (renew?: Renew<T>, makeObserver?: (store: T) => unknown[]) => {
-    const storeRenderObserveMap: StoreRenderObserveSubList<T> = {};
-    let makedProxy: null | T = null;
+  return (
+    renew?: Renew<T>,
+    makeObserver?: ((store: T) => unknown[]) | null,
+    userOption?: { cache?: boolean }
+  ) => {
+    const { cache } = Object.assign({}, DEFAULT_OPTION, userOption || {});
+
+    if (cache && renew && cacheMap.has(renew)) {
+      return cacheMap.get(renew)!;
+    }
+
+    const storeRenderObserveMap: StoreValue = {};
+    let makedProxy: { value: null | T } = { value: null };
+    let run: Run = () => {};
 
     storeRenderObserveList.push(storeRenderObserveMap);
 
     if (renew && makeObserver) {
-      makedProxy = updater<T>(
+      run = () => renew(makedProxy.value!);
+      makedProxy.value = updater<T>(
         value,
         allowInitSetting,
         storeRenderList,
-        () => renew(makedProxy!),
+        run,
         storeRenderObserveMap,
         storeRenderObserveList
       );
       allowInitSetting.value = true;
-      makeObserver(makedProxy);
+      makeObserver(makedProxy.value);
       allowInitSetting.value = false;
     }
 
-    if (!makedProxy) {
-      makedProxy = updater<T>(value, allowInitSetting, storeRenderList);
+    if (!makedProxy.value) {
+      makedProxy.value = updater<T>(value, allowInitSetting, storeRenderList);
 
       if (renew) {
-        storeRenderList.push(() => renew(makedProxy!));
+        run = () => renew(makedProxy.value!);
+        storeRenderList.add(run);
       }
     }
 
     if (renew) {
-      runFirstEmit<T>(
-        () => renew(makedProxy!),
-        storeRenderList,
-        storeRenderObserveMap
-      );
+      runFirstEmit(run, storeRenderList, storeRenderObserveMap);
+      cacheMap.set(renew, makedProxy.value);
     }
 
-    return makedProxy;
+    return makedProxy.value;
   };
 };
 
 const updater = <T extends { [key: string | symbol]: unknown }>(
   value: T,
   allowInitSetting: { value: boolean },
-  storeRenderList: Renew<T>[],
-  run?: () => boolean | AbortSignal | void,
-  storeRenderObserveMap?: StoreValue<T>,
-  storeRenderObserveList?: StoreRenderObserveSubList<T>[]
+  storeRenderList: Set<Run>,
+  run?: Run,
+  storeRenderObserveMap?: StoreValue,
+  storeRenderObserveList?: StoreValue[]
 ) => {
   const allowedAccessProp: (keyof T)[] = [];
 
   const result = new Proxy(value, {
     get(target: T, prop: keyof T) {
       if (run && storeRenderObserveMap && allowInitSetting.value) {
-        storeRenderObserveMap[prop] ??= [];
+        storeRenderObserveMap[prop] ??= new Set();
 
-        if (!storeRenderObserveMap[prop].includes(run)) {
-          storeRenderObserveMap[prop].push(run);
+        if (!storeRenderObserveMap[prop].has(run)) {
+          storeRenderObserveMap[prop].add(run);
           allowedAccessProp.push(prop);
         }
       }
@@ -89,14 +104,15 @@ const updater = <T extends { [key: string | symbol]: unknown }>(
       return null;
     },
     set(target, prop: keyof T, value) {
-      if (allowedAccessProp.includes(prop) || !run) {
+      if (target[prop] === value) {
+        return true;
+      } else if (allowedAccessProp.includes(prop) || !run) {
         target[prop] = value;
       } else {
         return true;
       }
 
       execDependentCallbacks(
-        result,
         storeRenderList,
         storeRenderObserveList,
         prop,
@@ -111,64 +127,59 @@ const updater = <T extends { [key: string | symbol]: unknown }>(
 };
 
 const execDependentCallbacks = <T>(
-  result: T,
-  storeRenderList: Renew<T>[],
-  storeRenderObserveList: StoreRenderObserveSubList<T>[] = [],
+  storeRenderList: Set<Run>,
+  storeRenderObserveList: StoreValue[] = [],
   prop: keyof T,
   run?: () => boolean | AbortSignal | void
 ) => {
-  const trashCollections: Renew<T>[] = [];
+  const trashCollections: Run[] = [];
 
-  trashCollections.push(...runWithtrashCollectUnit(storeRenderList, result));
+  trashCollections.push(...runWithtrashCollectUnit(storeRenderList));
 
   (storeRenderObserveList || []).forEach(storeRenderObserveMap => {
-    const renderObserveList: Renew<T>[] = run
-      ? storeRenderObserveMap[prop] || []
-      : [];
+    const renderObserveList: Set<Run> = run
+      ? storeRenderObserveMap[prop] || new Set<Run>()
+      : new Set<Run>();
 
-    trashCollections.push(
-      ...runWithtrashCollectUnit(renderObserveList, result)
-    );
+    trashCollections.push(...runWithtrashCollectUnit(renderObserveList));
 
-    removeTrashCollect(trashCollections, renderObserveList);
+    removeTrashCollect(trashCollections, [...renderObserveList.values()]);
   });
 
-  removeTrashCollect(trashCollections, storeRenderList);
+  removeTrashCollect(trashCollections, [...storeRenderList.values()]);
 };
 
-const removeTrashCollect = <T>(
-  trashCollections: Renew<T>[],
-  targetList: Renew<T>[]
-) => {
+const removeTrashCollect = (trashCollections: Run[], targetList: Run[]) => {
   trashCollections.forEach(deleteTarget => {
     targetList.splice(targetList.indexOf(deleteTarget), 1);
   });
 };
 
-const runWithtrashCollectUnit = <T>(storeRenderList: Renew<T>[], result: T) => {
-  const trashes: Renew<T>[] = [];
-  storeRenderList.forEach(renew => {
-    if (renew(result) === false) {
-      trashes.push(renew);
+const runWithtrashCollectUnit = (storeRenderList: Set<Run>) => {
+  const trashes: Run[] = [];
+  storeRenderList.forEach(run => {
+    if (run() === false) {
+      trashes.push(run);
     }
   });
   return trashes;
 };
 
-const runFirstEmit = <T>(
+const runFirstEmit = (
   run: () => boolean | void | AbortSignal,
-  storeRenderList: Renew<T>[],
-  storeRenderObserveMap: StoreValue<T>
+  storeRenderList: Set<Run>,
+  storeRenderObserveMap: StoreValue
 ) => {
   const renewResult = run();
 
   if (renewResult instanceof AbortSignal) {
     renewResult.addEventListener('abort', () => {
-      const renderObserveList: StoreValue<T> = storeRenderObserveMap || {};
+      const renderObserveList: StoreValue = storeRenderObserveMap || {};
 
-      storeRenderList.splice(storeRenderList.indexOf(run), 1);
+      storeRenderList.delete(run);
+
       Object.values(renderObserveList).forEach(item => {
-        item.splice(item.indexOf(run), 1);
+        item.delete(run);
       });
     });
   }
--- helper/src/tests/storeOneValue.tsx
+++ helper/src/tests/storeOneValue.tsx
@@ -3,6 +3,7 @@ import { store } from '@/hook/store';
 const testChangeRef = ref<null | (() => void)>(null);
 
 const subscribe = store<number>(0);
+const subscribeArr = store<number[]>([1]);
 
 let proxySubscribeChkValue = 0;
 const abortControl = new AbortController();
@@ -16,16 +17,21 @@ const proxyFirst = subscribe(store => {
 console.log(proxyFirst);
 
 const Component = mount(r => {
-  const local = subscribe(r);
+  const local = subscribe(r, null, { cache: false });
+  const localArr = subscribeArr(r);
 
   const click = () => {
     local.value += 1;
+    localArr.value = [...localArr.value, localArr.value.length + 1];
   };
   testChangeRef.value = click;
 
   return () => (
     <>
       <div>value: {local.value}</div>
+      {localArr.value.map(item => (
+        <span>{item}</span>
+      ))}
       <button type="text" onClick={click}>
         change count
       </button>
@@ -43,7 +49,7 @@ if (import.meta.vitest) {
 
   it('If the value is not an object, it is expanded to value and processed.', () => {
     expect(testWrap.outerHTML).toBe(
-      '<div><div>value: 0</div><button type="text">change count</button></div>'
+      '<div><div>value: 0</div><span>1</span><button type="text">change count</button></div>'
     );
     if (testChangeRef.value) {
       testChangeRef.value();
@@ -55,7 +61,7 @@ if (import.meta.vitest) {
   it('If the value is not an object, the event operates normally and the value increases.', () => {
     nextTick().then(() => {
       expect(testWrap.outerHTML).toBe(
-        '<div><div>value: 3</div><button type="text">change count</button></div>'
+        '<div><div>value: 3</div><span>1</span><span>2</span><span>3</span><span>4</span><button type="text">change count</button></div>'
       );
       expect(proxySubscribeChkValue).toBe(3);
     });
