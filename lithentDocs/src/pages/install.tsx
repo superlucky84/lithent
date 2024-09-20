c268c32303663d2c81e901090640f78e4843774e
superlucky84 <superlucky84@gmail.com>
2024-09-21 01:53:09 +0900

    chore: version up 1.12.0

--- lithentDocs/src/pages/install.tsx
+++ lithentDocs/src/pages/install.tsx
@@ -3,9 +3,9 @@ import { ContentHeader } from '@/components/contentHeader';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/hybrid.css';
 
-const code10 = `<script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/dist/lithent.umd.js"></script>
-<!--script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/helper/dist/lithentHelper.umd.js"></script-->
-<script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/ftags/dist/lithentFTags.umd.js"></script>
+const code10 = `<script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/dist/lithent.umd.js"></script>
+<!--script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/helper/dist/lithentHelper.umd.js"></script-->
+<script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/ftags/dist/lithentFTags.umd.js"></script>
 
 <div id="root"></div>
 
@@ -39,10 +39,10 @@ render(
 </script>
 `;
 
-const code7 = `<script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/dist/lithent.umd.js"></script>
-<!--script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/helper/dist/lithentHelper.umd.js"></script-->
-<script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/tag/dist/lithentTag.umd.js"></script>
-<!--script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/ftags/dist/lithentFTags.umd.js"></script-->
+const code7 = `<script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/dist/lithent.umd.js"></script>
+<!--script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/helper/dist/lithentHelper.umd.js"></script-->
+<script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/tag/dist/lithentTag.umd.js"></script>
+<!--script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/ftags/dist/lithentFTags.umd.js"></script-->
 
 <div id="root"></div>
 
@@ -97,7 +97,7 @@ const Component = mount((r, _props) => {
 const destroy = render(lTag\`<\${Component} />\`, document.getElementById('root'));
 `;
 
-const code9 = `<script src="https://cdn.jsdelivr.net/npm/lithent@1.11.2/tag/dist/lithentTag.umd.js"></script>;
+const code9 = `<script src="https://cdn.jsdelivr.net/npm/lithent@1.12.0/tag/dist/lithentTag.umd.js"></script>;
 const { lTag } = lithentTag;
 const destroy = render(lTag\`<\${Component} />\`, document.getElementById('root'));
 `;
@@ -260,27 +260,27 @@ export const Install = mount(() => {
       <div class="px-2 py-2 overflow-x-auto text-sm text-gray-50 border border-gray-200 border-dashed rounded border-gray-600 bg-black">
         <p> </p>
         <p>## umd</p>
-        <p>https://cdn.jsdelivr.net/npm/lithent@1.11.2/dist/lithent.umd.js</p>
+        <p>https://cdn.jsdelivr.net/npm/lithent@1.12.0/dist/lithent.umd.js</p>
         <p>
-          https://cdn.jsdelivr.net/npm/lithent@1.11.2/helper/dist/lithentHelper.umd.js
+          https://cdn.jsdelivr.net/npm/lithent@1.12.0/helper/dist/lithentHelper.umd.js
         </p>
         <p>
-          https://cdn.jsdelivr.net/npm/lithent@1.11.2/ftags/dist/lithentFTags.umd.js
+          https://cdn.jsdelivr.net/npm/lithent@1.12.0/ftags/dist/lithentFTags.umd.js
         </p>
         <p>
-          https://cdn.jsdelivr.net/npm/lithent@1.11.2/tag/dist/lithentTag.umd.js
+          https://cdn.jsdelivr.net/npm/lithent@1.12.0/tag/dist/lithentTag.umd.js
         </p>
         <p>&nbsp;</p>
         <p>## esm</p>
-        <p>https://cdn.jsdelivr.net/npm/lithent@1.11.2/dist/lithent.mjs</p>
+        <p>https://cdn.jsdelivr.net/npm/lithent@1.12.0/dist/lithent.mjs</p>
         <p>
-          https://cdn.jsdelivr.net/npm/lithent@1.11.2/helper/dist/lithentHelper.mjs
+          https://cdn.jsdelivr.net/npm/lithent@1.12.0/helper/dist/lithentHelper.mjs
         </p>
         <p>
-          https://cdn.jsdelivr.net/npm/lithent@1.11.2/ftags/dist/lithentFTags.mjs
+          https://cdn.jsdelivr.net/npm/lithent@1.12.0/ftags/dist/lithentFTags.mjs
         </p>
         <p>
-          https://cdn.jsdelivr.net/npm/lithent@1.11.2/tag/dist/lithentTag.mjs
+          https://cdn.jsdelivr.net/npm/lithent@1.12.0/tag/dist/lithentTag.mjs
         </p>
       </div>
       <div class="py-2 mb-4 overflow-x-auto text-sm text-gray-50 ">
