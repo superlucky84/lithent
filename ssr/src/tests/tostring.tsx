7ec0031ad1aefdf3eb4805aab0c8b84ca91c198f
superlucky84 <superlucky84@gmail.com>
2024-11-05 10:47:18 +0900

    feat: Create a test base for prop rendering and start implementation.

--- ssr/src/tests/tostring.tsx
+++ ssr/src/tests/tostring.tsx
@@ -3,8 +3,8 @@ import { renderToString } from '@/index';
 
 const Root = mount(_r => {
   return () => (
-    <div>
-      <span>123124</span>
+    <div style={{ border: '1px solid red' }}>
+      <span id="aa11">123124</span>
     </div>
   );
 });
