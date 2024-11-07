5939543487ecfaae850523cb421f923806b74db8
superlucky84 <superlucky84@gmail.com>
2024-11-07 13:37:17 +0900

    fix: ssr environment

--- ssr/src/index.ts
+++ ssr/src/index.ts
@@ -2,9 +2,15 @@ import { h } from 'lithent';
 import type { TagFunction } from 'lithent';
 import Test from '@/tests/tostring';
 export { renderToString } from '@/renderToString';
-import { hydration as hy } from '@/hydration';
-export const hydration = hy;
+import { hydration as _hydration } from '@/hydration';
+export const hydration = _hydration;
 
-export const run = (wrap: HTMLElement) => {
-  hydration(h(Test as TagFunction, {}), wrap);
-};
+let _run: ((wrap: HTMLElement) => void) | undefined;
+
+if (import.meta.env.MODE === 'development') {
+  _run = (wrap: HTMLElement) => {
+    hydration(h(Test as TagFunction, {}), wrap);
+  };
+}
+
+export const run = _run;
