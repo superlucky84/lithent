728e5e280a6cd4f1c5b6db1c1c2256377b091073
superlucky84 <superlucky84@gmail.com>
2024-11-19 19:06:03 +0900

    feat: Prototype for an array equality comparison function

--- createLithent/express/src/route.ts
+++ createLithent/express/src/route.ts
@@ -6,11 +6,32 @@ import { store } from 'lithent/helper';
 const pageModules = import.meta.glob('./pages/*.tsx');
 const cacheRender: { [key: string]: () => void } = {};
 
-function findPageModlueKey(pageModuleKeys: string[], key: string) {
-  console.log('KEY', key);
-  console.log('KEYSEGMENTLIST', key.split(/(?:\/|\.)/));
+/*
+function compareArraysWithUnderscore(arr1, arr2) {
+  // 배열 길이가 다르면 일치하지 않음
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-  console.log(pageModuleKeys);
+  // 각 항목 비교
+  for (let i = 0; i < arr1.length; i++) {
+    const item1 = arr1[i];
+    const item2 = arr2[i];
+
+    // `_`로 시작하지 않는 경우, 값이 정확히 같아야 함
+    if (!item1.startsWith('_') && !item2.startsWith('_') && item1 !== item2) {
+      return false; // 값이 다르면 false
+    }
+  }
+
+  // 모든 조건을 만족하면 true
+  return true;
+}
+*/
+
+function findPageModlueKey(pageModuleKeys: string[], key: string) {
+  const targetSegments = key.split(/(?:\/|\.)/);
+  console.log('TARGETSEGMENTS - ', targetSegments);
 
   pageModuleKeys.find(item => {
     const moduleSegmentList = item.split(/(?:\/|\.)/);
