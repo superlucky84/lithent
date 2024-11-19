1dd73d6349f04211767981dc7f4bec77e79ff284
superlucky84 <superlucky84@gmail.com>
2024-11-20 00:22:10 +0900

    feat: Dynamic Router Mapping Prototyping Completed

--- createLithent/express/src/route.ts
+++ createLithent/express/src/route.ts
@@ -1,60 +1,81 @@
 import { render as lRender, h } from 'lithent';
-import type { TagFunction } from 'lithent';
-import { renderWithHydration } from 'lithent/ssr';
 import { store } from 'lithent/helper';
 
 const pageModules = import.meta.glob('./pages/*.tsx');
-const cacheRender: { [key: string]: () => void } = {};
 
-/*
-function compareArraysWithUnderscore(arr1, arr2) {
+function compareArraysWithUnderscore(arr1: string[], arr2: string[]) {
+  const params: Record<string, string> = {};
   // 배열 길이가 다르면 일치하지 않음
   if (arr1.length !== arr2.length) {
-    return false;
+    return [false];
   }
 
-  // 각 항목 비교
   for (let i = 0; i < arr1.length; i++) {
     const item1 = arr1[i];
     const item2 = arr2[i];
 
-    // `_`로 시작하지 않는 경우, 값이 정확히 같아야 함
-    if (!item1.startsWith('_') && !item2.startsWith('_') && item1 !== item2) {
-      return false; // 값이 다르면 false
+    if (!item2.startsWith('_') && item1 !== item2) {
+      return [false]; // 값이 다르면 false
+    } else if (item2.startsWith('_')) {
+      params[item2.replace(/^_/, '')] = item1;
     }
   }
 
   // 모든 조건을 만족하면 true
-  return true;
+  return [true, params];
 }
-*/
 
 function findPageModlueKey(pageModuleKeys: string[], key: string) {
   const targetSegments = key.split(/(?:\/|\.)/);
-  console.log('TARGETSEGMENTS - ', targetSegments);
+  console.log('ts', targetSegments);
 
-  pageModuleKeys.find(item => {
+  let params: Record<string, string> = {};
+  const matchItem = pageModuleKeys.find(item => {
     const moduleSegmentList = item.split(/(?:\/|\.)/);
-    console.log('MODULESEGMENTLIST', moduleSegmentList);
+    const res = compareArraysWithUnderscore(targetSegments, moduleSegmentList);
+
+    params = res[1] as Record<string, string>;
+
+    return res[0];
   });
+
+  console.log('PARAM 0', params);
+  return { key: matchItem, params };
+}
+
+function parseQueryStringToMap(queryString: string) {
+  const params = new URLSearchParams(queryString);
+  const map: Record<string, string> = {};
+  params.forEach((value, key) => {
+    map[key] = value;
+  });
+  return map;
 }
 
 async function loadPage(dynamicPath: string) {
-  const key = `./pages${dynamicPath === '/' ? '/index' : dynamicPath}.tsx`;
-  findPageModlueKey(Object.keys(pageModules), key);
+  const orgPage = `./pages${dynamicPath === '/' ? '/index' : dynamicPath}.tsx`;
+  const comparePage = orgPage.replace(/\?[^\.]*/, '');
+  const queryOrg = orgPage.replace(/.*(\?[^\.]*).tsx/, '$1');
+  const query = /\?/.test(queryOrg) ? parseQueryStringToMap(queryOrg) : {};
+  const { key, params } = findPageModlueKey(
+    Object.keys(pageModules),
+    comparePage
+  );
 
-  if (cacheRender[key]) {
-    cacheRender[key]();
-  } else if (pageModules[key]) {
-    const res = await pageModules[key]();
-    //@ts-ignore
-    const render = () => lRender(h(res.default), document.documentElement);
-    render();
-    cacheRender[key] = render;
+  console.log('KEY', key, params);
+
+  if (key) {
+    if (pageModules[key]) {
+      const res = await pageModules[key]();
+
+      //@ts-ignore
+      lRender(h(res.default, { params, query }), document.documentElement);
+    }
+  } else {
+    location.href = comparePage;
   }
 }
 
-let init = true;
 export const routeAssign = store<{
   page: string;
   destroy: (() => void) | string;
@@ -71,7 +92,6 @@ export const routeRef = routeAssign(
       state.page !== prePage &&
       typeof state.destroy === 'function'
     ) {
-      init = false;
       state.destroy();
 
       loadPage(state.page);
@@ -84,23 +104,4 @@ export const routeRef = routeAssign(
 if (typeof window !== 'undefined') {
   //@ts-ignore
   window.routeRef = routeRef;
-}
-
-export function render(tagFunction: TagFunction) {
-  if (init) {
-    const destroy = renderWithHydration(tagFunction);
-
-    routeRef.destroy = destroy;
-
-    return typeof destroy === 'string'
-      ? destroy
-      : () => lRender(h(tagFunction, {}), document.documentElement);
-  }
-
-  const rRender = () => lRender(h(tagFunction, {}), document.documentElement);
-  const destroy = rRender();
-
-  routeRef.destroy = destroy;
-
-  return rRender;
 }
