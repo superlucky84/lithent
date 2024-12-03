161617d7b09abe1f3cfe18d74d029e1e56959f09
superlucky84 <superlucky84@gmail.com>
2024-12-04 01:27:20 +0900

    fix: boilerplate body layout

--- createLithent/express/src/layout.tsx
+++ createLithent/express/src/layout.tsx
@@ -28,7 +28,7 @@ const Layout = mount<{
         class={clsx(
           params.type
             ? `bg-pokemon-${params.type}`
-            : 'h-64 bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 via-violet-500 to-blue-500',
+            : 'bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 via-violet-500 to-blue-500',
           'flex items-center justify-center min-h-screen'
         )}
       >
