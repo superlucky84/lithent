d41ae7b8d0c8b6f60e60911e1adbef156f81fd47
superlucky84 <superlucky84@gmail.com>
2024-12-04 18:44:51 +0900

    feat: added watch-serve

--- createLithent/express/watch-serve.js
+++ createLithent/express/watch-serve.js
@@ -5,13 +5,11 @@ let serverProcess = spawn('node', ['--experimental-modules', 'server.js'], {
   stdio: 'inherit',
 });
 
-// server.js 파일 수정 감시
 const serverWatcher = chokidar.watch('server.js', {
   ignored: /(^|[\/\\])\../, // 숨김 파일 무시
   persistent: true,
 });
 
-// src 디렉토리에서 파일 추가/삭제 감시
 const srcWatcher = chokidar.watch('src', {
   ignored: /(^|[\/\\])\../, // 숨김 파일 무시
   persistent: true,
@@ -19,7 +17,6 @@ const srcWatcher = chokidar.watch('src', {
 
 let isSrcInitialScanComplete = false;
 
-// src 디렉토리 초기 스캔 완료 후 감시 시작
 srcWatcher.on('ready', () => {
   isSrcInitialScanComplete = true;
   console.log(
@@ -27,7 +24,6 @@ srcWatcher.on('ready', () => {
   );
 });
 
-// src 디렉토리 파일 추가/삭제 감지
 srcWatcher.on('add', path => {
   if (isSrcInitialScanComplete) {
     console.log(`File added in src: ${path}`);
@@ -40,7 +36,6 @@ srcWatcher.on('unlink', path => {
   }
 });
 
-// server.js 수정 감지
 serverWatcher.on('change', path => {
   console.log(`File changed: ${path}`);
   restartServer();
