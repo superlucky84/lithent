b9eaeaa51e3e8fb1222acfbf5f58198e2b2183bd
superlucky84 <superlucky84@gmail.com>
2024-12-05 19:00:12 +0900

    fix: sample template code

--- createLithent/express/src/pages/index.tsx
+++ createLithent/express/src/pages/index.tsx
@@ -48,7 +48,9 @@ const Index = mount(() => {
                     href="#"
                     onClick={(event: Event) => moveTypePage(event, name)}
                   >
-                    <span class="bg-pokemon-${name} inline-block text-sm font-bold tracking-wider uppercase mt-5 text-white px-2 py-1 rounded drop-shadow-md outline-2 outline-black outline-offset-1">
+                    <span
+                      class={`bg-pokemon-${name} inline-block text-sm font-bold tracking-wider uppercase mt-5 text-white px-2 py-1 rounded drop-shadow-md outline-2 outline-black outline-offset-1`}
+                    >
                       {name}
                     </span>
                   </a>
