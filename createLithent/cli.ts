47d632d6061ffdfcbfc90925642e6585dbaf85f9
superlucky84 <superlucky84@gmail.com>
2024-12-05 23:30:57 +0900

    fix: build and run

--- createLithent/cli.ts
+++ createLithent/cli.ts
@@ -1,5 +1,6 @@
-import process from 'node:process';
+#!/usr/bin/env node
 
+import process from 'node:process';
 import { createLithent } from './index.js';
 
 process.on('SIGINT', () => process.exit(0));
@@ -7,7 +8,10 @@ process.on('SIGTERM', () => process.exit(0));
 
 let argv = process.argv.slice(2).filter(arg => arg !== '--');
 
+createLithent(argv);
+/*
 createLithent(argv).then(
   () => process.exit(0),
   () => process.exit(1)
 );
+*/
