b5f3aaeafdb67da5924e380d7fca2acb4ed42c39
superlucky84 <superlucky84@gmail.com>
2025-01-30 10:47:09 +0900

    feat: Add a warning comment for HMR usage

--- createLithent/express/src/base/load.ts
+++ createLithent/express/src/base/load.ts
@@ -1,7 +1,15 @@
-import { h, componentUpdate, componentMap, replaceWDom } from 'lithent';
+import {
+  h,
+  componentUpdate,
+  // componentMap,
+  // replaceWDom
+} from 'lithent';
 import type { WDom, Props, TagFunction } from 'lithent';
 import { hydration } from 'lithent/ssr';
-import { makeRoute, makePathToKey } from '@/base/route';
+import {
+  makeRoute,
+  // makePathToKey
+} from '@/base/route';
 import { routeRef, pageModules } from '@/base/routeStore';
 import Layout from '@/layout';
 
@@ -46,34 +54,45 @@ export default async function load(
   hydration(LayoutWDom, document.documentElement);
 }
 
-if (import.meta.hot) {
-  pageModules.v = import.meta.glob('../pages/*.(tsx|mdx)');
-  import.meta.hot.accept(async () => {
-    console.log('Accept up resources...');
-
-    const dynamicPath = routeRef.page.value;
-    const rVDom = routeRef.rVDom.value;
-    const compKey = rVDom?.compKey;
-    const { key, id, query, params, origin } = makePathToKey(dynamicPath);
-
-    if (key && compKey) {
-      const currentVDom = componentMap.get(compKey)?.vd?.value;
-      const res = await pageModules.v[key]();
-      const preload = res.preload;
-
-      let initProp = null;
-      if (preload) {
-        initProp = await preload({ id, query, params, origin });
-      }
-      (globalThis as any).pagedata = initProp;
-
-      const props = { page: res.default, id, query, params };
-
-      try {
-        replaceWDom(Layout as TagFunction, props, [], currentVDom!);
-      } catch {
-        location.reload();
-      }
-    }
-  });
-}
+/**
+ * When comments are removed,
+ * HMR can be selectively applied to directories under /pages/.
+ *
+ * The following three APIs are required.
+ * Please uncomment them at the top as well.
+ *
+ * - componentMap
+ * - replaceWDom
+ * - makePathToKey
+ */
+//if (import.meta.hot) {
+//  pageModules.v = import.meta.glob('../pages/*.(tsx|mdx)');
+//  import.meta.hot.accept(async () => {
+//    console.log('Accept up resources...');
+//
+//    const dynamicPath = routeRef.page.value;
+//    const rVDom = routeRef.rVDom.value;
+//    const compKey = rVDom?.compKey;
+//    const { key, id, query, params, origin } = makePathToKey(dynamicPath);
+//
+//    if (key && compKey) {
+//      const currentVDom = componentMap.get(compKey)?.vd?.value;
+//      const res = await pageModules.v[key]();
+//      const preload = res.preload;
+//
+//      let initProp = null;
+//      if (preload) {
+//        initProp = await preload({ id, query, params, origin });
+//      }
+//      (globalThis as any).pagedata = initProp;
+//
+//      const props = { page: res.default, id, query, params };
+//
+//      try {
+//        replaceWDom(Layout as TagFunction, props, [], currentVDom!);
+//      } catch {
+//        location.reload();
+//      }
+//    }
+//  });
+//}
